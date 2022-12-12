e03aa89816cc02658c8078ec1a761679635191bd
Juan de Dios <jddios837@gmail.com>
2022-12-12 14:39:41 -0500

    Working in the challenge 10

--- 10-Challenge/index.js
+++ 10-Challenge/index.js
@@ -1,5 +1,23 @@
 function checkJump(heights) {
-    return false
+    const left = heights.splice(0, heights.indexOf(Math.max(...heights)));
+
+    const goodForLeft = left
+        .slice(1)
+        .every((l, i) => l >= left[i]);
+
+    const goofForRight = heights
+        .slice(1)
+        .every((h, i) => h <= heights[i]);
+
+    return goodForLeft && goofForRight && !!left.length && heights.length > 1
 }
+
+console.log(checkJump([1, 2, 2, 2, 1])) // TRUE
+console.log(checkJump([2, 2, 2, 2])) // FALSE
+console.log(checkJump([1, 1, 1, 1, 1, 1, 1, 1, 2, 1])) // True
+console.log(checkJump([1, 2, 3, 1, 3, 1])) // FALSE
+console.log(checkJump([1, 2, 3])) // FALSE
+console.log(checkJump([0, 1, 0])) // TRUE
+console.log(checkJump([1, 3, 2, 5, 4, 3, 2, 1])) // FALSE
 
 module.exports = checkJump;
--- 10-Challenge/index.test.js
+++ 10-Challenge/index.test.js
@@ -4,4 +4,60 @@ describe('Challenge 10', () => {
    test('Test #01 - expect return type boolean', () => {
       expect(typeof checkJump([1])).toBe('boolean');
    });
+
+   test('Test #02 - Heights [1, 2, 1] True', () => {
+      expect(checkJump([1, 2, 1])).toBe(true);
+   });
+
+   test('Test #03 - Heights [1, 3, 8, 5, 2] True', () => {
+      expect(checkJump([1, 3, 8, 5, 2])).toBe(true);
+   });
+
+   test('Test #04 - Heights [1, 7, 3, 5] False', () => {
+      expect(checkJump([1, 7, 3, 5])).toBe(false);
+   });
+
+   test('Test #05 - Heights [1, 2, 3, 2, 1] True', () => {
+      expect(checkJump([1, 2, 3, 2, 1])).toBe(true);
+   });
+
+   test('Test #06 - Heights [1, 2, 2, 2, 1] True', () => {
+      expect(checkJump([1, 2, 2, 2, 1])).toBe(true);
+   });
+
+   test('Test #07 - Heights [0, 1, 0] True', () => {
+      expect(checkJump([0, 1, 0])).toBe(true);
+   });
+
+   test('Test #08 - Heights [2, 2, 2, 2] False', () => {
+      expect(checkJump([2, 2, 2, 2])).toBe(false);
+   });
+
+   test('Test #09 - Heights [1, 2, 3] False', () => {
+      expect(checkJump([1, 2, 3])).toBe(false);
+   });
+
+   test('Test #10 - Heights [1, 2, 3, 2, 1, 2, 3] False', () => {
+      expect(checkJump([1, 2, 3, 2, 1, 2, 3])).toBe(false);
+   });
+
+   test('Test #11 - Heights [1, 1000, 900, 800] True', () => {
+      expect(checkJump([1, 1000, 900, 800])).toBe(true);
+   });
+
+   test('Test #12 - Heights [1, 1000, 100, 800] False', () => {
+      expect(checkJump([1, 1000, 100, 800])).toBe(false);
+   });
+
+   test('Test #13 - Heights [1, 1, 1, 1, 1, 1, 1, 1, 2, 1] True', () => {
+      expect(checkJump([1, 1, 1, 1, 1, 1, 1, 1, 2, 1])).toBe(true);
+   });
+
+   test('Test #14 - Heights [1, 2, 3, 1, 3, 1] False', () => {
+      expect(checkJump([1, 2, 3, 1, 3, 1])).toBe(false);
+   });
+
+   test('Test #15 - Heights [1, 3, 2, 5, 4, 3, 2, 1] False', () => {
+      expect(checkJump([1, 3, 2, 5, 4, 3, 2, 1])).toBe(false);
+   });
 });
